7980f5df3cc5f18f6c48068a0c9c11f950d98701
Hansel Dunlop <hansel@interpretthis.org>
2014-11-05 08:58:38 +0000

    Adds a couple of  very basic tests

---
+++ tests/test_browser_test_case.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+from test_helpers import BrowserTestCase
+
+
+class BrowserTestCaseTest(TestCase):
+
+    class SubClassed(BrowserTestCase):
+
+        def do_nothing(self):
+            pass
+
+    def test_start_browser_when_given_unsupported_driver(self):
+        bc = self.SubClassed("do_nothing")
+
+        with self.assertRaises(ValueError):
+            bc.start_browser(driver="NoReal")
+
+        self.assertEqual(bc._browsers, [])
+
+    def test_browser_is_cleaned_up_afterwards(self):
+        bc = self.SubClassed("do_nothing")
+        bc.start_browser("Firefox")
+
+        bc.doCleanups()
+
+        with self.assertRaises(Exception):
+            bc.title
