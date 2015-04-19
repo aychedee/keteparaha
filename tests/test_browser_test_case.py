8609eefe036ad6ae7344d3b12f5c1dc9a8ea5aff
aychedee <hansel@interpretthis.org>
2015-04-19 22:54:26 +0100

    Smoketest for start browser

--- tests/test_browser_test_case.py
+++ tests/test_browser_test_case.py
@@ -1,7 +1,11 @@
 from unittest import TestCase
 from mock import call, patch, Mock
 
-from keteparaha.browser import BrowserTestCase, snapshot_on_error
+from keteparaha.browser import (
+    BrowserTestCase,
+    HeadlessBrowserTestCase,
+    snapshot_on_error
+)
 
 
 class SubClassed(BrowserTestCase):
@@ -74,3 +78,49 @@ class SnapshotOnErrorDecorator(TestCase):
                 call('/my-path/test-id_browser-0_page-3.png')
             ]
         )
+
+    def test_handles_errors_figuring_out_window_size(self, mock_makedirs):
+        mock_browser = Mock()
+        mock_browser.find_element_by_tag_name.side_effect = KeyError
+
+        def test_func(self, *args, **kwargs):
+            raise TypeError('a type error')
+
+        class ExampleTest(object):
+            browsers = [mock_browser]
+            SNAPSHOT_PATH = '/my-path'
+
+            def id(self):
+                return 'test-id'
+
+        decorated = snapshot_on_error(test_func)
+
+        with self.assertRaises(TypeError):
+            decorated(ExampleTest())
+
+        self.assertEqual(
+            mock_makedirs.call_args, call(ExampleTest.SNAPSHOT_PATH))
+        self.assertEqual(
+            mock_browser.find_element_by_tag_name.call_args, call('body'))
+        self.assertEqual(mock_browser.execute_script.call_args_list, [])
+        self.assertEqual(
+            mock_browser.get_screenshot_as_file.call_args_list, [])
+
+
+
+@patch('keteparaha.browser.webdriver')
+class HeadlessBrowserTestCaseTest(TestCase):
+
+    def test_start_browser(self, mock_webdriver):
+
+        class SampleTC(HeadlessBrowserTestCase):
+            _display = Mock()
+
+            def runTest(self):
+                pass
+
+        tc = SampleTC()
+
+        tc.start_browser()
+
+        self.assertEqual(tc._driver, mock_webdriver.Firefox.return_value)
