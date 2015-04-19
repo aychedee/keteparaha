0cccd9a9914a4842ca630aebb21650aebec7e7c4
aychedee <hansel@interpretthis.org>
2015-04-19 14:06:50 +0100

    Test for retry decorator

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -23,3 +23,41 @@ class IgnoreTest(TestCase):
 
         with self.assertRaises(RuntimeError):
             wrapped(*a, **k)
+
+
+class RetryTest(TestCase):
+
+    def test_retries_function_given_number_of_times(self):
+        a, k = (1, 2, 3), {'1': 1, '2': 2, '3': 3}
+
+        globals()['counter'] = 5
+
+        def test_func(*args, **kwargs):
+            global counter
+            if counter > 0:
+                print counter
+                counter -= 1
+                raise Exception()
+            return args, kwargs
+
+        wrapped = retry(test_func, (Exception), attempts=6)
+
+        self.assertEqual((wrapped(*a, **k)), (a, k))
+
+    def test_raises_error_if_function_still_erroring(self):
+        a, k = (1, 2, 3), {'1': 1, '2': 2, '3': 3}
+
+        globals()['counter'] = 5
+
+        def test_func(*args, **kwargs):
+            global counter
+            if counter > 0:
+                print counter
+                counter -= 1
+                raise Exception()
+            return args, kwargs
+
+        wrapped = retry(test_func, (Exception), attempts=5)
+
+        with self.assertRaises(Exception):
+            wrapped(*a, **k)
